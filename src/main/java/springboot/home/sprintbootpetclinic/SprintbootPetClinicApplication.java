0f783e56bae8924be76d84fefb7fd901d277d830
lnguyen9621 <lnguyen9621@gmail.com>
2020-02-18 20:45:53 -0700

    initial files and code of the pet clinic project

---
+++ src/main/java/springboot/home/sprintbootpetclinic/SprintbootPetClinicApplication.java
@@ -0,0 +1,13 @@
+package springboot.home.sprintbootpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintbootPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/home/sprintbootpetclinic/SprintbootPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.home.sprintbootpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
